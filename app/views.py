a82b2615d9b24110a14bb03451e01e0cbcba3033
Vinay Keerthi <ktvkvinaykeerthi@gmail.com>
2017-01-31 19:34:23 +0530

    Added submission rules.

--- app/views.py
+++ app/views.py
@@ -9,6 +9,7 @@ from .forms import LoginForm
 def secret():
     user = {"nickname": "Boss"}
     return render_template('secret.html', title="secret", user=user)
+
 @app.route("/")
 @app.route("/index")
 def index():
@@ -25,8 +26,12 @@ def index():
         ]
     return render_template('index.html', title="home", user=user, posts=posts)
 
-@app.route("/login")
+@app.route("/login", methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for OpenID="{}", remember_me={}'.format(form.openid,data, str(form.remember_me.data)))
+        return redirect('/index')
+        
     return render_template('login.html', title='Sign In', form=form)
 
